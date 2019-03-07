edbbad81ce474d421623caa73e5fb50c152815fe
Taraspetrenko04 <Taraspetrenko04@gmail.com>
2019-03-07 12:15:22 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+
+console.log(y);
+let x = 1;
+var y = 2;
+const pi = 3.14;
+// objects
+let persone = {
+    name: "Taras",
+    age: 29,
+    isMarried: true
+}
+console.log(persone.age);
+console.log(persone["isMarried"]);
+// Arrays
+// let arr = ["I", "HE", "SHE"];
+// console.log(arr[1]);
+
+// alert ("hello");
+// let answer = confirm("are u married");
+// console.log(answer);
+
+// let age = prompt("Are you 18 years old?", "no")
+// console.log(typeof(age));
+
+// let arr = [1,2,3];
+// alert(typeof(arr));
+// alert(typeof(persone));
+//если перед выражением поставить плюс то данные переопределятся в строку:
+// let xy = +prompt("how are you?");
+// console.log(typeof(xy + "persone"));
+// let incr = 10;
+// let decr = 10;
+// // incr++;
+// // decr--;
+// console.log(++incr);
+// console.log(--decr);
+// console.log(5%2); // 1 (5/2= 2*2+1) 
+// console.log("2" ==  2);// true
+// console.log("2" ===  2);// false
+// let am = true;
+// let no = true;
+// let ni = false;
+// console.log(am && no); // true
+// console.log(am && ni); // false
+// console.log(am || ni); // true
+let v = false;
+console.log(!v);//true
+
